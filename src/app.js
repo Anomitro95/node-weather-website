b87b9cb5a8ef62e3dbcd29de63526cc2b734d10c
Anomitro95 <anomitro1995@gmail.com>
2022-01-12 18:28:17 +0530

    deleted stuff

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.static(publicDirectory))
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title :'Weather App Home',       
+        title :'Weather App',       
         name :'Anomi'
     })
 })
